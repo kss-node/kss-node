27e67358e32f941da862a73775aba773595200ae
JohnAlbin <virtually.johnalbin@gmail.com>
2014-10-13 04:19:20 +0800

    Add handlebars helpers tests. Fixes #86

--- test/kss-node.js
+++ test/kss-node.js
@@ -29,12 +29,66 @@ suite('Command Line Interface', function() {
 		});
 	});
 
-	suite('Option: --helpers', function() {
-		test('Should load additional Handlerbars helpers', function(done) {
-		  // @TODO: Doing a "cat" of the output file seems inelegant.
-			exec('bin/kss-node test/fixtures-styles/with-include test/output --template test/fixtures-styles/template --helpers test/fixtures-styles/template/helpers; cat test/output/index.html', function(err, stdout, stderr) {
+	suite('Handlebars helper', function() {
+		// @TODO: Doing a "cat" of the output file seems inelegant.
+		test('Should load additional Handlerbars helpers with --helpers option', function(done) {
+			exec('rm -r test/output; bin/kss-node test/fixtures-styles/with-include test/output --template test/fixtures-styles/template --helpers test/fixtures-styles/template/helpers; cat test/output/index.html', function(err, stdout, stderr) {
 				assert.ok(/The file containing the Handlebars helper was loaded\./g.test(stdout));
 				assert.ok(/Handlerbars helper loaded into template!/g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{section [arg]}}', function(done) {
+			exec('cat test/output/section-3.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars Section Helper Test 3/g.test(stdout));
+				assert.ok(/Section 3 has been successfully loaded\./g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{eachSection [arg]}}', function(done) {
+			exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars eachSection Helper Test 2.1.3/g.test(stdout));
+				assert.ok(/Handlebars eachSection Helper Test 2.1.4/g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{eachRoot}}', function(done) {
+			exec('cat test/output/index.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars eachRoot Helper Test 2/g.test(stdout));
+				assert.ok(/Handlebars eachRoot Helper Test 3/g.test(stdout));
+				assert.notEqual(/Handlebars eachRoot Helper Test 2.1.3/g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{ifDepth [arg]}}', function(done) {
+			exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars ifDepth Helper Test 2.1</g.test(stdout));
+				assert.notEqual(/Handlebars ifDepth Helper Test 2.1.3</g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{unlessDepth [arg]}}', function(done) {
+			exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars unlessDepth Helper Test 2.1.3</g.test(stdout));
+				assert.notEqual(/Handlebars unlessDepth Helper Test 2.1</g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{eachModifier}}', function(done) {
+			exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars eachModifier Helper: :hover/g.test(stdout));
+				assert.ok(/Handlebars eachModifier Helper: \.stars\-given</g.test(stdout));
+				assert.ok(/Handlebars eachModifier Helper: \.stars\-given:hover/g.test(stdout));
+				assert.ok(/Handlebars eachModifier Helper: \.disabled/g.test(stdout));
+				done();
+			});
+		});
+		test('Should load Handlerbars helper: {{modifierMarkup}}', function(done) {
+			exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+				assert.ok(/Handlebars modifierMarkup Helper: pseudo\-class\-hover/g.test(stdout));
+				assert.ok(/Handlebars modifierMarkup Helper: stars\-given</g.test(stdout));
+				assert.ok(/Handlebars modifierMarkup Helper: stars\-given pseudo\-class\-hover/g.test(stdout));
+				assert.ok(/Handlebars modifierMarkup Helper: disabled/g.test(stdout));
 				done();
 			});
 		});
