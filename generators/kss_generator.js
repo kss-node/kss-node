77540991d5881c72862b772434b2841068e8db33
JohnAlbin <virtually.johnalbin@gmail.com>
2015-02-16 22:07:57 +0800

    Add KssGenerator.clone(). #138

--- generators/kss_generator.js
+++ generators/kss_generator.js
@@ -2,7 +2,8 @@
    See kss_example_generator.js for how to implement a generator.
    ************************************************************** */
 
-var KssGenerator;
+var KssGenerator,
+  wrench = require('wrench');
 
 /**
  * Export the KssGenerator object.
@@ -12,5 +13,30 @@ var KssGenerator;
 module.exports = KssGenerator = function () {
   if (!(this instanceof KssGenerator)) {
     return new KssGenerator();
+  }
+};
+
+/**
+ * Clone a template's files.
+ *
+ * This method is fairly simple; it copies one directory to the specified
+ * location. An instance of KssGenerator does not need to override this
+ * method, but it can if it needs to do something more complicated.
+ */
+KssGenerator.prototype.clone = function(templatePath, destinationPath) {
+  try {
+    error = wrench.copyDirSyncRecursive(
+      templatePath,
+      destinationPath,
+      {
+        forceDelete: false,
+        excludeHiddenUnix: true
+      }
+    );
+    if (error) {
+      throw error;
+    }
+  } catch (e) {
+    throw 'Error! This folder already exists: ' + destinationPath;
   }
 };
