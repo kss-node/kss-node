2f96bab434b11b6e4a0fe29aa8373d3cf1d18e9d
JohnAlbin <virtually.johnalbin@gmail.com>
2015-02-19 08:01:04 +0800

    Move KssHandlebarsGenerator to generators/handlebars. #138

---
+++ generator.js
@@ -0,0 +1,7 @@
+// Convenience script to load the base KSS Generator.
+//
+// Usage:
+// var KssGenerator = require('kss/generator');
+// var KssHandlebarsGenerator = require('kss/generators/handlebarsGenerator');
+
+module.exports = require('./generators/kss_generator.js');
