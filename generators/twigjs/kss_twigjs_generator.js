0681f054d5d599757ef5c78b7869a2c16b3e955f
Z Snow <rossetti@Waka.local>
2015-03-30 17:28:48 +1100

    Define variable after undefining redefinition.

--- generators/twigjs/kss_twigjs_generator.js
+++ generators/twigjs/kss_twigjs_generator.js
@@ -173,8 +173,8 @@ KssTwigJSGenerator.generate = function(styleguide) {
 
     // Accumulate all of the sections' first indexes
     // in case they don't have a root element.
-    currentRoot = sections[i].reference().split(/(?:\.|\s+\-\s+)/)[0],
-    found = false;
+    currentRoot = sections[i].reference().split(/(?:\.|\s+\-\s+)/)[0];
+    var found = false;
     for ( var j in sectionRoots ) {
       if ( sectionRoots[j].reference == currentRoot ) {
         found = true;
