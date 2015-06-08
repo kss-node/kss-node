96b7a729fd2a35141fcb7e6b284e89e283996622
JohnAlbin <virtually.johnalbin@gmail.com>
2015-06-08 12:49:45 +0800

    Add tests for missing parent directory of destination. #193

--- test/kss-node.js
+++ test/kss-node.js
@@ -15,14 +15,14 @@ suite('Command Line Interface', function() {
 
   suite('Option: --source', function() {
     test('Should read source directory from option', function(done) {
-      exec('bin/kss-node --verbose --source test/fixtures-styles/with-include --destination test/output', function(err, stdout, stderr) {
+      exec('bin/kss-node --verbose --source test/fixtures-styles/with-include --destination test/output/nested', function(err, stdout, stderr) {
         assert.ok(/\* KSS Source  : .+test\/fixtures\-styles\/with\-include/g.test(stdout), 'Read --source option');
         assert.strictEqual(/no styleguide overview generated/g.test(stdout), false, 'Styleguide homepage not generated from empty file');
         done();
       });
     });
     test('Should not declare success if source directory is empty', function(done) {
-      exec('bin/kss-node --verbose --source test/fixtures-styles/empty-source --destination test/output', function(err, stdout, stderr) {
+      exec('bin/kss-node --verbose --source test/fixtures-styles/empty-source --destination test/output/nested', function(err, stdout, stderr) {
         assert.ok(/\* KSS Source  : .+test\/fixtures\-styles\/empty\-source/g.test(stdout), 'Read --source option');
         assert.strictEqual(/Style guide generation completed successfully/g.test(stdout), false, 'Success incorrectly declared');
         assert.ok(/No KSS documentation discovered in source files./g.test(stdout), 'Warning about no KSS docs given');
@@ -30,14 +30,14 @@ suite('Command Line Interface', function() {
       });
     });
     test('Warn if homepage content is not found', function(done) {
-      exec('bin/kss-node --source test/fixtures-styles/missing-homepage --destination test/output', function(err, stdout, stderr) {
+      exec('bin/kss-node --source test/fixtures-styles/missing-homepage --destination test/output/nested', function(err, stdout, stderr) {
         assert.ok(/no homepage content found/g.test(stdout), 'Warning about no homepage content found');
         assert.strictEqual(/no styleguide overview generated/g.test(stdout), false, 'Styleguide homepage not generated from missing file');
         done();
       });
     });
     test('Should read multiple source directories from option', function(done) {
-      exec('bin/kss-node --verbose --source test/fixtures-styles/with-include --source test/fixtures-styles/empty-source --destination test/output', function(err, stdout, stderr) {
+      exec('bin/kss-node --verbose --source test/fixtures-styles/with-include --source test/fixtures-styles/empty-source --destination test/output/nested', function(err, stdout, stderr) {
         assert.ok(/\* KSS Source  : .+test\/fixtures\-styles\/with\-include, .+test\/fixtures\-styles\/empty\-source/g.test(stdout), 'Read multiple --source options');
         assert.strictEqual(/Style guide generation completed successfully/g.test(stdout), true, 'Styleguide generated from multiple sources');
         done();
@@ -47,8 +47,8 @@ suite('Command Line Interface', function() {
 
   suite('Option: --destination', function() {
     test('Should read destination directory from option', function(done) {
-      exec('bin/kss-node --verbose test/fixtures-styles/with-include --destination test/output', function(err, stdout, stderr) {
-        assert.ok(/\* Destination : .+test\/output/g.test(stdout), 'Read --destination option');
+      exec('bin/kss-node --verbose test/fixtures-styles/with-include --destination test/output/nested', function(err, stdout, stderr) {
+        assert.ok(/\* Destination : .+test\/output\/nested/g.test(stdout), 'Read --destination option');
         done();
       });
     });
@@ -56,7 +56,7 @@ suite('Command Line Interface', function() {
 
   suite('Option: --custom', function() {
     test('Should read custom properties from option', function(done) {
-      exec('rm -r test/output; bin/kss-node test/fixtures-styles/with-include test/output --template test/fixtures-styles/template --custom custom --custom custom2; cat test/output/section-4.html', function(err, stdout, stderr) {
+      exec('rm -r test/output; bin/kss-node test/fixtures-styles/with-include test/output/nested --template test/fixtures-styles/template --custom custom --custom custom2; cat test/output/nested/section-4.html', function(err, stdout, stderr) {
         assert.ok(/"custom" property: This is the first custom property\./g.test(stdout), 'Read --custom option');
         assert.ok(/"custom2" property: This is the second custom property\./g.test(stdout), 'Read second --custom option');
         done();
@@ -76,28 +76,28 @@ suite('Command Line Interface', function() {
   suite('Handlebars helper', function() {
     // @TODO: Doing a "cat" of the output file seems inelegant.
     test('Should load additional Handlerbars helpers with --helpers option', function(done) {
-      exec('rm -r test/output; bin/kss-node test/fixtures-styles/with-include test/output --template test/fixtures-styles/template --helpers test/fixtures-styles/template/helpers; cat test/output/index.html', function(err, stdout, stderr) {
+      exec('rm -r test/output; bin/kss-node test/fixtures-styles/with-include test/output/nested --template test/fixtures-styles/template --helpers test/fixtures-styles/template/helpers; cat test/output/nested/index.html', function(err, stdout, stderr) {
         assert.ok(/The file containing the Handlebars helper was loaded\./g.test(stdout), 'Handlebars helpers js file read');
         assert.ok(/Handlerbars helper loaded into template!/g.test(stdout), 'Handlebars helpers loaded');
         done();
       });
     });
     test('Should load Handlerbars helper: {{section [arg]}}', function(done) {
-      exec('cat test/output/section-3.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-3.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars Section Helper Test 3/g.test(stdout), 'test 1');
         assert.ok(/Section 3 has been successfully loaded\./g.test(stdout), 'test 2');
         done();
       });
     });
     test('Should load Handlerbars helper: {{eachSection [arg]}}', function(done) {
-      exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-2.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars eachSection Helper Test 2.1.3/g.test(stdout), 'test 1');
         assert.ok(/Handlebars eachSection Helper Test 2.1.4/g.test(stdout), 'test 2');
         done();
       });
     });
     test('Should load Handlerbars helper: {{eachRoot}}', function(done) {
-      exec('cat test/output/index.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/index.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars eachRoot Helper Test 2/g.test(stdout), 'test 1');
         assert.ok(/Handlebars eachRoot Helper Test 3/g.test(stdout), 'test 2');
         assert.strictEqual(/Handlebars eachRoot Helper Test 2.1.3/g.test(stdout), false, 'test 3');
@@ -105,21 +105,21 @@ suite('Command Line Interface', function() {
       });
     });
     test('Should load Handlerbars helper: {{ifDepth [arg]}}', function(done) {
-      exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-2.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars ifDepth Helper Test 2.1</g.test(stdout), 'test 1');
         assert.strictEqual(/Handlebars ifDepth Helper Test 2.1.3</g.test(stdout), false, 'test 2');
         done();
       });
     });
     test('Should load Handlerbars helper: {{unlessDepth [arg]}}', function(done) {
-      exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-2.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars unlessDepth Helper Test 2.1.3</g.test(stdout), 'test 1');
         assert.strictEqual(/Handlebars unlessDepth Helper Test 2.1</g.test(stdout), false, 'test 2');
         done();
       });
     });
     test('Should load Handlerbars helper: {{eachModifier}}', function(done) {
-      exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-2.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars eachModifier Helper: :hover/g.test(stdout), 'test 1');
         assert.ok(/Handlebars eachModifier Helper: \.stars\-given</g.test(stdout), 'test 2');
         assert.ok(/Handlebars eachModifier Helper: \.stars\-given:hover/g.test(stdout), 'test 3');
@@ -128,7 +128,7 @@ suite('Command Line Interface', function() {
       });
     });
     test('Should load Handlerbars helper: {{{markup}}}', function(done) {
-      exec('cat test/output/section-2.html', function(err, stdout, stderr) {
+      exec('cat test/output/nested/section-2.html', function(err, stdout, stderr) {
         assert.ok(/Handlebars markup Helper: pseudo\-class\-hover/g.test(stdout), 'test 1');
         assert.ok(/Handlebars markup Helper: stars\-given</g.test(stdout), 'test 2');
         assert.ok(/Handlebars markup Helper: stars\-given pseudo\-class\-hover/g.test(stdout), 'test 3');
