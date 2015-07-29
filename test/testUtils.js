c9c41b1a3ae210676ec8930ed3a4313b1c6d976e
JohnAlbin <virtually.johnalbin@gmail.com>
2015-07-29 13:50:14 +0800

    Simplify tests by removing testUtils.sectionQueryArray().

--- test/testUtils.js
+++ test/testUtils.js
@@ -12,18 +12,6 @@ module.exports = {
     return path.join(__dirname, 'fixtures', subpath);
   },
 
-  sectionQueryArray: function(done, query, options, testFunction) {
-    kss.traverse(this.fixtures(), options || {}, function(err, styleguide) {
-      err.should.not.be.Error();
-      var sections = styleguide.section(query);
-
-      sections.should.be.an.Array('query did not return an array');
-      sections.length.should.not.be.equal(0, 'query returned empty array');
-      testFunction(sections);
-      done();
-    });
-  },
-
   traverseFixtures: function(options, cb) {
     kss.traverse(this.fixtures(), options, function(err, styleguide) {
       err.should.not.be.Error();
--- test/test_kss_styleguide.js
+++ test/test_kss_styleguide.js
@@ -6,9 +6,6 @@
 var kss = require('../index.js'),
   testUtils = require('./testUtils');
 
-var query = '',
-  options = {mask: 'sections-queries.less'};
-
 describe('KssStyleguide object API', function() {
   before(function(done) {
     var self = this;
@@ -38,63 +35,28 @@ describe('KssStyleguide object API', function() {
 
     context('given no arguments', function() {
       it('should return only referenced sections', function(done) {
-        testUtils.sectionQueryArray(done, query, options, function(sections) {
-          sections.map(function(section) {
-            section.data.should.have.property('reference');
-          });
+        this.styleguide.section().map(function(section) {
+          section.data.should.have.property('reference');
         });
+        done();
       });
 
       it('should return all referenced sections', function(done) {
-        testUtils.sectionQueryArray(done, query, options, function(sections) {
-          var results = [],
-            expected = [
-              '4', '4.1',
-              '4.1.1', '4.1.1.1', '4.1.1.2',
-              '4.1.2', '4.1.2.2',
-              '8'
-            ];
-          sections.map(function(section) {
-            results.push(section.reference());
-          });
-          results.should.be.eql(expected);
+        var results = [],
+          expected = [
+            '4', '4.1',
+            '4.1.1', '4.1.1.1', '4.1.1.2',
+            '4.1.2', '4.1.2.2',
+            '8',
+            '9', '9.1', '9.1.1', '9.2', '9.3', '9.4', '9.5', '9.10', '9.11', '9.100',
+            'alpha', 'alpha.alpha', 'alpha.alpha.alpha', 'alpha.beta', 'alpha.delta', 'alpha.epsilon', 'alpha.gamma', 'alpha-bet',
+            'WordKeys.Base.Link', 'WordKeys.Components', 'WordKeys.Components.Message', 'WordKeys.Components.Tabs', 'WordKeys.Forms.Button', 'WordKeys.Forms.Input'
+          ];
+        this.styleguide.section().map(function(section) {
+          results.push(section.reference());
         });
-      });
-
-      it('should return all "word key" sections', function(done) {
-        testUtils.sectionQueryArray(done, query, {mask: 'property-styleguide-word-keys.less'}, function(sections) {
-          var results = [],
-            expected = [
-              'WordKeys.Base.Link',
-              'WordKeys.Components',
-              'WordKeys.Components.Message',
-              'WordKeys.Components.Tabs',
-              'WordKeys.Forms.Button',
-              'WordKeys.Forms.Input'
-            ];
-          sections.map(function(section) {
-            results.push(section.reference());
-          });
-          results.should.be.eql(expected);
-        });
-      });
-
-      it('should return all "word phrases" sections', function(done) {
-        testUtils.sectionQueryArray(done, query, {mask: 'property-styleguide-word-phrases.less'}, function(sections) {
-          var results = [],
-            expected = [
-              'WordPhrases - Base - Link',
-              'WordPhrases - Components',
-              'WordPhrases - Components - Message box',
-              'WordPhrases - Components - Tabs',
-              'WordPhrases - Forms - Button',
-              'WordPhrases - Forms - Input field'
-            ];
-          sections.map(function(section) {
-            results.push(section.reference());
-          });
-          results.should.be.eql(expected);
-        });
+        results.should.be.eql(expected);
+        done();
       });
     });
 
@@ -155,46 +117,43 @@ describe('KssStyleguide object API', function() {
 
     context('given string queries', function() {
       it('should return 1 level of descendants when given "4.x"', function(done) {
-        testUtils.sectionQueryArray(done, '4.x', options, function(sections) {
-          sections.map(function(section) {
-            section.reference().should.equal('4.1');
-            section.header().should.equal('DEPTH OF 2');
-          });
-          sections.length.should.equal(1);
+        var sections = this.styleguide.section('4.x');
+        sections.map(function(section) {
+          section.reference().should.equal('4.1');
+          section.header().should.equal('DEPTH OF 2');
         });
+        sections.length.should.equal(1);
+        done();
       });
 
       it('should return 1 level of descendants when given "4.1.x"', function(done) {
-        testUtils.sectionQueryArray(done, '4.1.x', options, function(sections) {
-          var results,
-            expected = ['4.1.1', '4.1.2'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['4.1.1', '4.1.2'];
+        results = this.styleguide.section('4.1.x').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return 2 levels of descendants when given "4.x.x"', function(done) {
-        testUtils.sectionQueryArray(done, '4.x.x', options, function(sections) {
-          var results,
-            expected = ['4.1', '4.1.1', '4.1.2'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['4.1', '4.1.1', '4.1.2'];
+        results = this.styleguide.section('4.x.x').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "4.1" and all levels of descendants when given "4.1.*"', function(done) {
-        testUtils.sectionQueryArray(done, '4.1.*', options, function(sections) {
-          var results,
-            expected = ['4.1', '4.1.1', '4.1.1.1', '4.1.1.2', '4.1.2', '4.1.2.2'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['4.1', '4.1.1', '4.1.1.1', '4.1.1.2', '4.1.2', '4.1.2.2'];
+        results = this.styleguide.section('4.1.*').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should not find "alpha" section when given a query for "alp.*"', function(done) {
@@ -210,122 +169,113 @@ describe('KssStyleguide object API', function() {
       });
 
       it('should return numeric sections in order', function(done) {
-        testUtils.sectionQueryArray(done, '9.x', {mask: 'sections-order.less'}, function(sections) {
-          var results,
-            expected = ['9.1', '9.2', '9.3', '9.4', '9.5', '9.10', '9.11', '9.100'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['9.1', '9.2', '9.3', '9.4', '9.5', '9.10', '9.11', '9.100'];
+        results = this.styleguide.section('9.x').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "word key" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, 'alpha.x', {mask: 'sections-order.less'}, function(sections) {
-          var results,
-            expected = ['alpha.alpha', 'alpha.beta', 'alpha.delta', 'alpha.epsilon', 'alpha.gamma'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['alpha.alpha', 'alpha.beta', 'alpha.delta', 'alpha.epsilon', 'alpha.gamma'];
+        results = this.styleguide.section('alpha.x').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "word key" sections with dashes in the name', function(done) {
-        testUtils.sectionQueryArray(done, 'alpha-bet.*', {mask: 'sections-order.less'}, function(sections) {
-          sections.map(function(section) {
-            section.reference().should.equal('alpha-bet');
-          });
-          sections.length.should.equal(1);
+        var sections = this.styleguide.section('alpha-bet.*');
+        sections.map(function(section) {
+          section.reference().should.equal('alpha-bet');
         });
+        sections.length.should.equal(1);
+        done();
       });
 
       it('should return "word phrase" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, 'beta.x', {mask: 'sections-order-word-phrases.less'}, function(sections) {
-          var results,
-            expected = ['beta - alpha', 'beta - beta', 'beta - delta', 'beta - epsilon', 'beta - gamma'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['beta - alpha', 'beta - beta', 'beta - delta', 'beta - epsilon', 'beta - gamma'];
+        results = this.styleguideWordPhrases.section('beta.x').map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
     });
 
     context('given regex queries', function() {
       it('should return "4" and all levels of descendants when given /4.*/', function(done) {
-        testUtils.sectionQueryArray(done, /4.*/, options, function(sections) {
-          var results,
-            expected = ['4', '4.1', '4.1.1', '4.1.1.1', '4.1.1.2', '4.1.2', '4.1.2.2'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['4', '4.1', '4.1.1', '4.1.1.1', '4.1.1.2', '4.1.2', '4.1.2.2'];
+        results = this.styleguide.section(/4.*/).map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "4" when given /4/', function(done) {
-        testUtils.sectionQueryArray(done, /4/, options, function(sections) {
-          sections.map(function(section) {
-            section.reference().should.equal('4');
-          });
-          sections.length.should.equal(1);
+        var sections = this.styleguide.section(/4/);
+        sections.map(function(section) {
+          section.reference().should.equal('4');
         });
+        sections.length.should.equal(1);
+        done();
       });
 
       it('should return numeric sections in order', function(done) {
-        testUtils.sectionQueryArray(done, /9.*/, {mask: 'sections-order.less'}, function(sections) {
-          var results,
-            expected = ['9', '9.1', '9.1.1', '9.2', '9.3', '9.4', '9.5', '9.10', '9.11', '9.100'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['9', '9.1', '9.1.1', '9.2', '9.3', '9.4', '9.5', '9.10', '9.11', '9.100'];
+        results = this.styleguide.section(/9.*/).map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "word key" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, /alpha\..*/, {mask: 'sections-order.less'}, function(sections) {
-          var results,
-            expected = ['alpha.alpha', 'alpha.alpha.alpha', 'alpha.beta', 'alpha.delta', 'alpha.epsilon', 'alpha.gamma'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['alpha.alpha', 'alpha.alpha.alpha', 'alpha.beta', 'alpha.delta', 'alpha.epsilon', 'alpha.gamma'];
+        results = this.styleguide.section(/alpha\..*/).map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return "word phrase" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, /beta - .*/, {mask: 'sections-order-word-phrases.less'}, function(sections) {
-          var results,
-            expected = ['beta - alpha', 'beta - alpha - alpha', 'beta - beta', 'beta - delta', 'beta - epsilon', 'beta - gamma'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['beta - alpha', 'beta - alpha - alpha', 'beta - beta', 'beta - delta', 'beta - epsilon', 'beta - gamma'];
+        results = this.styleguideWordPhrases.section(/beta - .*/).map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return weighted "word phrase" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, /gamma - .*/, {mask: 'sections-order-word-phrases.less'}, function(sections) {
-          var results,
-            expected = ['gamma - alpha', 'gamma - alpha - delta', 'gamma - alpha - gamma', 'gamma - alpha - beta', 'gamma - alpha - alpha', 'gamma - beta', 'gamma - gamma', 'gamma - delta', 'gamma - epsilon'];
-          results = sections.map(function(section) {
-            return section.reference();
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['gamma - alpha', 'gamma - alpha - delta', 'gamma - alpha - gamma', 'gamma - alpha - beta', 'gamma - alpha - alpha', 'gamma - beta', 'gamma - gamma', 'gamma - delta', 'gamma - epsilon'];
+        results = this.styleguideWordPhrases.section(/gamma - .*/).map(function(section) {
+          return section.reference();
         });
+        results.should.eql(expected);
+        done();
       });
 
       it('should return autoincrement values for "word phrase" sections in order', function(done) {
-        testUtils.sectionQueryArray(done, /gamma - .*/, {mask: 'sections-order-word-phrases.less'}, function(sections) {
-          var results,
-            expected = ['2.1', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.2', '2.3', '2.4', '2.5'];
-          results = sections.map(function(section) {
-            return section.data.autoincrement;
-          });
-          results.should.eql(expected);
+        var results,
+          expected = ['2.1', '2.1.1', '2.1.2', '2.1.3', '2.1.4', '2.2', '2.3', '2.4', '2.5'];
+        results = this.styleguideWordPhrases.section(/gamma - .*/).map(function(section) {
+          return section.data.autoincrement;
         });
+        results.should.eql(expected);
+        done();
       });
     });
   });
