abac2a8d1ed3ca087f88670bd6b2a007abf40961
JohnAlbin <virtually.johnalbin@gmail.com>
2016-02-15 07:02:48 +0800

    Add 100% test coverage for cli.js. #267

--- test/test_cli.js
+++ test/test_cli.js
@@ -78,9 +78,35 @@ describe('Command Line Interface', function() {
 
   describe('given --config option', function() {
     it('should load configuration from JSON file', function() {
-      return kssNode('--config test/fixtures/cli-option-config.json').then(function(result) {
+      return kssNode('--config test/fixtures/cli-option-config-source-array.json').then(function(result) {
         expect(result.error).to.not.exist;
         expect(result.stdout).to.include(successMessage);
+      });
+    });
+
+    it('should add the first unnamed as the source directory', function() {
+      let source = helperUtils.fixtures('missing-homepage');
+      return kssNode('--config test/fixtures/cli-option-config.json --verbose ' + source).then(function(result) {
+        expect(result.error).to.not.exist;
+        expect(result.stdout).to.include('* KSS Source  : ' + source + ', ' + helperUtils.fixtures('with-include'));
+        expect(result.stdout).to.include(successMessage);
+
+        let source2 = helperUtils.fixtures('includes');
+        return kssNode('--config test/fixtures/cli-option-config-source-array.json --verbose ' + source2).then(function(result) {
+          expect(result.error).to.not.exist;
+          expect(result.stdout).to.include('* KSS Source  : ' + source2 + ', ' + helperUtils.fixtures('with-include') + ', ' + helperUtils.fixtures('missing-homepage'));
+          expect(result.stdout).to.include(successMessage);
+        });
+      });
+    });
+  });
+
+  describe('given --demo option', function() {
+    it('should create a demo style guide', function() {
+      return kssNode('--demo --destination ' + helperUtils.fixtures('../output/nested')).then(function(result) {
+        expect(result.error).to.not.exist;
+        expect(result.stdout).to.include(successMessage);
+        expect(result.stdout).to.include('WELCOME to the kss-node demo! We\'ve turned on the --verbose flag so you can see what kss-node is doing.');
       });
     });
   });
