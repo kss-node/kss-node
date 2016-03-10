edce0bcd32759166675150f9811b8b417a4adc4e
JohnAlbin <virtually.johnalbin@gmail.com>
2016-03-10 13:33:15 +0800

    Convert test to use Promisified fs.readFile().

--- test/test_kss_builder_handlebars.js
+++ test/test_kss_builder_handlebars.js
@@ -6,31 +6,16 @@ const cli = require('../lib/cli'),
   mockStream = require('mock-utf8-stream');
 
 describe('KssBuilderHandlebars builder (default)', function() {
-  before(function(done) {
+  before(function() {
     let stdout = new mockStream.MockWritableStream(),
       stderr = new mockStream.MockWritableStream();
 
     stdout.startCapture();
     stderr.startCapture();
 
-    // Create a closure with the file name stored.
-    let fileCounter;
     this.files = {};
-    let fileReader = fileName => {
-      return (error, data) => {
-        if (error) {
-          throw error;
-        }
 
-        this.files[fileName] = data;
-        fileCounter -= 1;
-        if (!fileCounter) {
-          done();
-        }
-      };
-    };
-
-    cli({
+    return cli({
       stdout: stdout,
       stderr: stderr,
       argv: ['node', 'bin/kss-node', 'test/fixtures/with-include', 'test/output/nested', '--builder', 'test/fixtures/builder', '--helpers', 'test/fixtures/builder/helpers']
@@ -40,52 +25,50 @@ describe('KssBuilderHandlebars builder (default)', function() {
     }).then(result => {
       expect(result).to.not.be.instanceOf(Error);
       this.stdout = stdout.capturedData;
-      let files = [
-        'index',
-        'section-2',
-        'section-3'
-      ];
-      fileCounter = files.length;
-      files.forEach(function(file) {
-        fs.readFile(path.join(__dirname, 'output', 'nested', file + '.html'), 'utf8', fileReader(file));
-      });
+      return Promise.all(
+        [
+          'index',
+          'section-2',
+          'section-3'
+        ].map(fileName => {
+          return fs.readFileAsync(path.join(__dirname, 'output', 'nested', fileName + '.html'), 'utf8').then(data => {
+            this.files[fileName] = data;
+          });
+        })
+      );
     });
   });
 
   describe('given --helpers option', function() {
-    it('should load additional Handlebars helpers', function(done) {
+    it('should load additional Handlebars helpers', function() {
       expect(this.files.index).to.include('Handlebars helper loaded into template!');
-      done();
     });
   });
 
   describe('KssBuilderBaseHandlebar\'s Handlebars helpers', function() {
-    it('should load Handlebars helper: {{{markup}}}', function(done) {
+    it('should load Handlebars helper: {{{markup}}}', function() {
       expect(this.files['section-2']).to.include('Handlebars markup Helper: pseudo-class-hover');
       expect(this.files['section-2']).to.include('Handlebars markup Helper: stars-given<');
       expect(this.files['section-2']).to.include('Handlebars markup Helper: stars-given pseudo-class-hover');
       expect(this.files['section-2']).to.include('Handlebars markup Helper: disabled');
       expect(this.files['section-2']).to.include('Nested Handlebars partial part 1:part 2 of Nested Handlebars partial');
       expect(this.files['section-2']).to.include('Test of Handlebars partial data');
-      done();
     });
   });
 
   describe('builder\'s Handlebars helpers', function() {
-    it('should load Handlebars helper: {{section [arg]}}', function(done) {
+    it('should load Handlebars helper: {{section [arg]}}', function() {
       expect(this.files['section-3']).to.include('Handlebars Section Helper Test 3');
       expect(this.files['section-3']).to.include('Section 3 has been successfully loaded.');
-      done();
     });
 
-    it('should load Handlebars helper: {{eachSection [arg]}}', function(done) {
+    it('should load Handlebars helper: {{eachSection [arg]}}', function() {
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper Test 2.1.3');
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper Test 2.1.4');
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper: #each modifiers: :hover');
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper: #each modifiers: .stars-given<');
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper: #each modifiers: .stars-given:hover');
       expect(this.files['section-2']).to.include('Handlebars eachSection Helper: #each modifiers: .disabled');
-      done();
     });
   });
 });
